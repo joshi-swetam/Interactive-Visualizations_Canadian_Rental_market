93fc5edfdb92b2217bb1e3d4dcf7cf9895f17906
Sakshi Dalal <8.sakshi@gmail.com>
2023-06-12 05:39:28 -0400

    Updated files

--- Sakshi/static/js/script.js
+++ Sakshi/static/js/script.js
@@ -1,9 +1,53 @@
+// populate dropdown menus and create initial plots
 d3.json("/data").then(function(data) {
-    // Extract data for plotting
-    let values = data.map(d => d.RentalInformation.AverageRent);
-    let labels = data.map(d => d.Location.Province);
 
-    // Create a bar plot
+    let years = [...new Set(data.map(d => d.Year))];
+    let provinces = [...new Set(data.map(d => d.Location.Province))];
+
+    // populate dropdown menus
+    let yearSelect = document.getElementById('yearSelect');
+    let provinceSelect = document.getElementById('provinceSelect');
+
+    years.forEach(year => {
+        let option = document.createElement('option');
+        option.text = year;
+        option.value = year;
+        yearSelect.appendChild(option);
+    });
+
+    provinces.forEach(province => {
+        let option = document.createElement('option');
+        option.text = province;
+        option.value = province;
+        provinceSelect.appendChild(option);
+    });
+
+    // create initial plots
+    updatePlots(data, years[0], provinces[0]);
+});
+
+// update plots when dropdown selection changes
+document.getElementById('yearSelect').addEventListener('change', updateSelection);
+document.getElementById('provinceSelect').addEventListener('change', updateSelection);
+
+// update plots based on selected year and province
+function updateSelection() {
+    let selectedYear = document.getElementById('yearSelect').value;
+    let selectedProvince = document.getElementById('provinceSelect').value;
+    
+    d3.json("/data").then(function(data) {
+        updatePlots(data, selectedYear, selectedProvince);
+    });
+}
+
+// create/update bar plot and pie chart
+function updatePlots(data, year, province) {
+    let filteredData = data.filter(d => d.Year == year && d.Location.Province == province);
+    
+    let values = filteredData.map(d => d.RentalInformation.AverageRent);
+    let labels = filteredData.map(d => d.Location.Neighbourhood);
+
+    // update bar plot
     var trace1 = {
         x: labels,
         y: values,
@@ -11,12 +55,12 @@ d3.json("/data").then(function(data) {
     };
 
     var layout1 = {
-        title: 'Average Rent by Province',
+        title: `Average Rent by Neighbourhood in ${province}, ${year}`,
     };
 
     Plotly.newPlot('bar', [trace1], layout1);
 
-    // Create a pie chart
+    // update pie chart
     var trace2 = {
         values: values,
         labels: labels,
@@ -24,9 +68,8 @@ d3.json("/data").then(function(data) {
     };
 
     var layout2 = {
-        title: 'Average Rent by Province',
+        title: `Average Rent by Neighbourhood in ${province}, ${year}`,
     };
 
     Plotly.newPlot('pie', [trace2], layout2);
-});
-
+}
